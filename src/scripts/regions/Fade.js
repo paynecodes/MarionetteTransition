680d1d343fa008224d1257c7da71b01748793dfb
Jarrod Payne <jpayne@webotomy.com>
2014-06-06 12:41:13 -0400

    Adding a Fade region with new opacity animation
    Signed-off-by: Jarrod Payne <jpayne@webotomy.com>

---
+++ src/scripts/regions/Fade.js
@@ -0,0 +1,36 @@
+define(['jquery', 'underscore', 'backbone.marionette', 'gsap.tweenlite', 'gsap.cssplugin', 'MarionetteTransition', 'animations/opacity'], function($, _, Marionette, TweenLite, CSSPlugin, MarionetteTransition, opacity) {
+    'use strict';
+
+    var Fade = MarionetteTransition.extend({
+        prepareCommonOptions: function(options) {
+            options.duration = .4;
+        },
+        prepareNewView: function(view, options) {
+            var setOptions = {};
+
+            view.$el.addClass('mt-view');
+
+            _.extend(setOptions, { 'opacity': '0' });
+
+            TweenLite.set(view.$el, setOptions);
+        },
+        prepareOldView: function(view, options) {
+            var setOptions = {};
+
+            view.$el.addClass('mt-view');
+
+            _.extend(setOptions, { 'opacity': '1' });
+
+            TweenLite.set(view.$el, setOptions);
+        },
+        enterAnimation: function(view, options) {
+            return opacity(view.$el, _.extend(options, { 'opacity': '1' }));
+        },
+        exitAnimation: function(view, options) {
+            return opacity(view.$el, _.extend(options, { 'opacity': '0' }));
+        }
+    });
+
+    return Fade;
+
+});
