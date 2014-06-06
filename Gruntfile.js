e42cb983df2c5c1ed51ce79958b7400e9ce7942a
Jarrod Payne <jpayne@webotomy.com>
2014-06-06 19:13:05 -0400

    Cleaned up Gruntfile
    Signed-off-by: Jarrod Payne <jpayne@webotomy.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,71 +29,10 @@ module.exports = function (grunt) {
         watch: {
             js: {
                 files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-                tasks: ['jshint'],
-                options: {
-                    livereload: true
-                }
-            },
-            jstest: {
-                files: ['test/spec/{,*/}*.js'],
-                tasks: ['test:watch']
+                tasks: ['jshint']
             },
             gruntfile: {
                 files: ['Gruntfile.js']
-            },
-            compass: {
-                files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-                tasks: ['compass:server', 'autoprefixer']
-            },
-            styles: {
-                files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
-                tasks: ['newer:copy:styles', 'autoprefixer']
-            },
-            livereload: {
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                },
-                files: [
-                    '<%= yeoman.app %>/{,*/}*.html',
-                    '.tmp/styles/{,*/}*.css',
-                    '<%= yeoman.app %>/images/{,*/}*.{gif,jpeg,jpg,png,svg,webp}'
-                ]
-            }
-        },
-
-        // The actual grunt server settings
-        connect: {
-            options: {
-                port: 9000,
-                livereload: 35729,
-                // Change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
-            },
-            livereload: {
-                options: {
-                    open: true,
-                    base: [
-                        '.tmp',
-                        '<%= yeoman.app %>'
-                    ]
-                }
-            },
-            test: {
-                options: {
-                    port: 9001,
-                    base: [
-                        '.tmp',
-                        'test',
-                        '<%= yeoman.app %>'
-                    ]
-                }
-            },
-            dist: {
-                options: {
-                    open: true,
-                    base: '<%= yeoman.dist %>',
-                    livereload: false
-                }
             }
         },
 
@@ -122,165 +61,50 @@ module.exports = function (grunt) {
                 'Gruntfile.js',
                 '<%= yeoman.app %>/scripts/{,*/}*.js',
                 '!<%= yeoman.app %>/scripts/vendor/*',
-                'test/spec/{,*/}*.js'
             ]
-        },
-
-
-        // Mocha testing framework configuration options
-        mocha: {
-            all: {
-                options: {
-                    run: true,
-                    urls: ['http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html']
-                }
-            }
-        },
-
-
-
-
-        // Compiles Sass to CSS and generates necessary files if requested
-        compass: {
-            options: {
-                sassDir: '<%= yeoman.app %>/styles',
-                cssDir: '.tmp/styles',
-                javascriptsDir: '<%= yeoman.app %>/scripts',
-                importPath: '<%= yeoman.app %>/bower_components'
-            },
-            dist: {
-                options: {
-                    sassDir: '<%= yeoman.app %>/styles',
-                    cssDir: '.tmp/styles'
-                }
-            },
-            server: {
-                options: {
-                    debugInfo: true
-                }
-            }
-        },
-
-        // Add vendor prefixed styles
-        autoprefixer: {
-            options: {
-                browsers: ['last 1 version']
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '.tmp/styles/',
-                    src: '{,*/}*.css',
-                    dest: '.tmp/styles/'
-                }]
-            }
-        },
-
-        cssmin: {
-            dist: {
-                files: {
-                    '<%= yeoman.dist %>/styles/kitchen-sink.min.css': [
-                        '.tmp/styles/{,*/}*.css',
-                        '<%= yeoman.app %>/styles/{,*/}*.css'
-                    ]
-                }
-            }
-        },
-        // uglify: {
-        //     dist: {
-        //         files: {
-        //             '<%= yeoman.dist %>/scripts/scripts.js': [
-        //                 '<%= yeoman.dist %>/scripts/scripts.js'
-        //             ]
-        //         }
-        //     }
-        // },
-        concat: {
-            dist: {}
         },
 
         // Copies remaining files to places other tasks can use
         copy: {
-            dist: {
-                files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: '<%= yeoman.app %>',
-                    dest: '<%= yeoman.dist %>',
-                    src: [
-                        'styles/{,**/}*.scss',
-                        '.tmp/styles'
-                    ]
-                }]
-            },
-            styles: {
+            amd: {
                 expand: true,
                 dot: true,
-                cwd: '<%= yeoman.app %>/styles',
-                dest: '.tmp/styles/',
-                src: '{,*/}*.css'
+                cwd: '<%= yeoman.app %>/scripts/',
+                dest: '<%= yeoman.dist %>/scripts/amd',
+                src: [
+                    '{,*/}*.js',
+                ]
             }
-        },
-
-
-        // Generates a custom Modernizr build that includes only the tests you
-        // reference in your app
-        modernizr: {
-            devFile: '<%= yeoman.app %>/bower_components/modernizr/modernizr.js',
-            outputFile: '<%= yeoman.dist %>/bower_components/modernizr/modernizr.js',
-            files: [
-                '<%= yeoman.dist %>/scripts/{,*/}*.js',
-                '<%= yeoman.dist %>/styles/{,*/}*.css',
-                '!<%= yeoman.dist %>/scripts/vendor/*'
-            ],
-            uglify: true
-        },
-
-        // Run some tasks in parallel to speed up build process
-        concurrent: {
-            server: [
-                'compass:server',
-                'copy:styles'
-            ],
-            test: [
-                'copy:styles'
-            ],
-            dist: [
-                'compass:dist',
-                'copy:styles'
-            ]
         },
 
         requirejs: {
             options: {
                 baseUrl: '<%= yeoman.app %>/scripts',
                 paths: {
-                    "AnimatedRegion": "AnimatedRegion",
-                    "Transitioner": "Transitioner",
-                    "jquery": "../bower_components/jquery/dist/jquery",
-                    "underscore": "../bower_components/underscore/underscore",
-                    "backbone": "../bower_components/backbone/backbone",
-                    "backbone.marionette": "../bower_components/backbone.marionette/lib/backbone.marionette",
-                    "almond": "../bower_components/almond/almond",
+                    'jquery': '../bower_components/jquery/dist/jquery',
+                    'underscore': '../bower_components/underscore/underscore',
+                    'backbone': '../bower_components/backbone/backbone',
+                    'backbone.marionette': '../bower_components/backbone.marionette/lib/backbone.marionette',
+                    'almond': '../bower_components/almond/almond',
                     'gsap.tweenlite': '../bower_components/gsap/src/uncompressed/TweenLite',
                     'gsap.cssplugin': '../bower_components/gsap/src/uncompressed/plugins/CSSPlugin'
                 },
                 shim: {
                     'gsap.cssplugin': {
-                        exports: 'CSSPlugin'
+                        exports: 'CSSPlugin',
                         deps: ['gsap.tweenlite']
                     },
                     'gsap.tweenlite': {
                         exports: 'TweenLite'
                     }
                 },
-                include: ['almond', 'AnimatedRegion', 'Transitioner'],
-                exclude: ['jquery', 'underscore', 'backbone', 'backbone.marionette'],
+                include: ['almond', 'MarionetteTransition'],
+                exclude: ['jquery', 'underscore', 'backbone', 'backbone.marionette', 'gsap.tweenlite', 'gsap.cssplugin'],
                 out: '<%= yeoman.dist %>/scripts/MarionetteTransition.min.js',
                 optimize: 'uglify',
                 wrap:{
-                    startFile:"<%= yeoman.app %>/fragments/wrap-start.frag",
-                    endFile:"<%= yeoman.app %>/fragments/wrap-end.frag"
+                    startFile:'<%= yeoman.app %>/fragments/wrap-start.frag',
+                    endFile:'<%= yeoman.app %>/fragments/wrap-end.frag'
                 }
             },
             dist: {},
@@ -293,56 +117,15 @@ module.exports = function (grunt) {
         }
     });
 
-
-    grunt.registerTask('serve', function (target) {
-        if (target === 'dist') {
-            return grunt.task.run(['build', 'connect:dist:keepalive']);
-        }
-
-        grunt.task.run([
-            'clean:server',
-            'concurrent:server',
-            'autoprefixer',
-            'connect:livereload',
-            'watch'
-        ]);
-    });
-
-    grunt.registerTask('server', function () {
-        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-        grunt.task.run(['serve']);
-    });
-
-    grunt.registerTask('test', function(target) {
-        if (target !== 'watch') {
-            grunt.task.run([
-                'clean:server',
-                'concurrent:test',
-                'autoprefixer',
-            ]);
-        }
-
-        grunt.task.run([
-            'connect:test',
-            'mocha'
-        ]);
-    });
-
     grunt.registerTask('build', [
         'clean:dist',
-        'concurrent:dist',
-        'autoprefixer',
-        'concat',
-        'cssmin',
-        'copy:dist',
+        'copy:amd',
         'requirejs:dist',
-        'requirejs:distNoMin',
-        'modernizr'
+        'requirejs:distNoMin'
     ]);
 
     grunt.registerTask('default', [
         'newer:jshint',
-        'test',
         'build'
     ]);
 };
