91df09ad5159de2bc50ef1980e12a76b3d834fdf
Jarrod Payne <jpayne@webotomy.com>
2014-06-06 11:20:40 -0400

    Adding FullPageModal region with vertical transition
    Signed-off-by: Jarrod Payne <jpayne@webotomy.com>

---
+++ src/scripts/animations/verticalSlideToPosition.js
@@ -0,0 +1,12 @@
+define(['gsap.tweenlite', 'gsap.cssplugin'], function(TweenLite, CSSPlugin) {
+    'use strict';
+
+    return function(el, position, options) {
+      return TweenLite.to(el, options.duration, {
+          y: position,
+          opacity: options.opacity || '1',
+          paused: options.paused || true
+      });
+    };
+
+});
---
+++ src/scripts/regions/FullPageModal.js
@@ -0,0 +1,34 @@
+define(['jquery', 'underscore', 'backbone.marionette', 'gsap.tweenlite', 'gsap.cssplugin', 'MarionetteTransition', 'animations/verticalSlideToPosition'], function($, _, Marionette, TweenLite, CSSPlugin, MarionetteTransition, verticalSlideToPosition) {
+    'use strict';
+
+    var FullPageModal = MarionetteTransition.extend({
+        prepareCommonOptions: function(options) {
+            options.distance = $('body').height();
+            options.duration = .4;
+        },
+        prepareRegionContainer: function() {
+            this.$el.addClass('open');
+            this.listenToOnce(this, 'transition:end:out close', function() {
+                this.$el.removeClass('open');
+            });
+        },
+        prepareNewView: function(view, options) {
+            var setOptions = {};
+
+            view.$el.addClass('mt-view');
+
+            _.extend(setOptions, { 'y': options.distance });
+
+            TweenLite.set(view.$el, setOptions);
+        },
+        enterAnimation: function(view, options) {
+            return verticalSlideToPosition(view.$el, 0, options);
+        },
+        exitAnimation: function(view, options) {
+            return verticalSlideToPosition(view.$el, options.distance, options);
+        }
+    });
+
+    return FullPageModal;
+
+});
